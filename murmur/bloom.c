670fd2455f570dda6c11815790f4c9a6dd2c5123
ttommyy <tunger10@gmail.com>
2015-08-11 21:55:14 -0400

    finished add and lookup

--- murmur/bloom.c
+++ murmur/bloom.c
@@ -1,14 +1,122 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#define DEBUG 0
+
+//Parameters
+//Table size
+#define TBSZ 256
+//Number Hash Fns
+#define NH 5
+//First Seed, Assuming Consecutive, 
+#define FS 0
+
 
 uint32_t murmur3_32(const char *key, uint32_t len, uint32_t seed);
 
-int main(){
-	long key;
-	for (int i = 0; i < 10; ++i)
-	{
-		uint32_t hash = murmur3_32((char *)&key, sizeof(long), i);
-		printf("%u\n",hash);
+void add(int * h, unsigned long key){
+	//Hashes key with NH hash functions.
+	//Sets corresponding bits of h to reflect
+	//key's membership in h. 
+
+	uint32_t val;
+	//Set bits specified by hash fns. 
+	for (int i = FS; i < FS + NH; ++i){
+		val = murmur3_32((char *)&key, sizeof(long), i) % TBSZ;
+		h[val] = 1;
 	}
-  return 0;
+
+	if(DEBUG)
+		printf("inserted %lu \n", key);
 }
+
+int lookup(int * h, unsigned long key){
+	uint32_t val;
+	int found = 0;
+	for (int i = FS; i < FS + NH; ++i){
+		val = murmur3_32((char *)&key, sizeof(long), i) % TBSZ;
+		
+		//The hash fn says val must be set.
+		if(h[val]){
+			//If below is true, all bits check out.
+			if (i == FS + NH-1){
+				found = 1;
+				if (DEBUG)
+					printf("found %lu\n", key);
+			}
+			//Check next bit.
+			continue;
+		//h[val] was not set, key cannot be in h.
+		}else {
+			if(DEBUG){
+				printf("failed to find %lu\n", key);
+			}
+			return 0;
+		}
+	}
+	return found;
+}
+
+int main(){
+	//the table
+	int * h;
+	h = calloc (TBSZ, sizeof(int));
+	if(!h){
+		printf("failed to allocate h\n");
+		exit(1); 
+	}
+
+	unsigned long key = 5;
+	add(h, key);
+	if(lookup(h, key))
+		printf("found %lu\n", key);
+	else
+		printf("did not find %lu\n", key);
+	key = 4;
+	if(lookup(h, key))
+		printf("found %lu\n", key);
+	else
+		printf("did not find %lu\n", key);
+	add(h,9);
+	key = 9;
+	if(lookup(h, key))
+		printf("found %lu\n", key);
+	else
+		printf("did not find %lu\n", key);
+
+	key = 5;
+	if(lookup(h, key))
+		printf("found %lu\n", key);
+	else
+		printf("did not find %lu\n", key);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
